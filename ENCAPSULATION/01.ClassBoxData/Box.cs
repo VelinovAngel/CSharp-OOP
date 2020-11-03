6ca43ff0d93cb91aec88b94a86703f9958a001fe
Angel <kakarum1992@gmail.com>
2020-11-03 18:49:11 +0100

    ClassBoxData exercise OOP

---
+++ ENCAPSULATION/01.ClassBoxData/Box.cs
@@ -0,0 +1,81 @@
+﻿using System;
+namespace _01.ClassBoxData
+{
+    public class Box
+    {
+        private const string MSG_EXC_INV_COORDINATE = "{0} cannot be zero or negative.";
+
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length , double width, double height)
+        {
+            this.Lenght = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Lenght
+        {
+            get
+            {
+                return this.length;
+            }
+            private set
+            {
+                Validation(value, nameof(Lenght));              
+
+                this.length = value;
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+            private set
+            {
+                //if (value <= 0)
+                //{
+                //    throw new ArgumentException(String.Format(MSG_EXC_INV_COORDINATE, nameof(Width)));
+                //}
+
+                Validation(value,nameof(Width));
+
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+            private set
+            {
+                //if (value <= 0)
+                //{
+                //    throw new ArgumentException(String.Format(MSG_EXC_INV_COORDINATE, nameof(Height)));
+                //}
+
+                Validation(value, nameof(Height));
+
+                this.height = value;
+            }
+        }
+
+        private void Validation(double side , string paramName)
+        {
+            if (side <= 0)
+            {
+                throw new ArgumentException(String.Format(MSG_EXC_INV_COORDINATE, paramName));
+            }
+        }
+
+
+    }
+}
---
+++ ENCAPSULATION/01.ClassBoxData/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01.ClassBoxData
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new Box(length, width, height);
+
+            }
+            catch (ArgumentException aex)
+            {
+                Console.WriteLine(aex.Message);
+            }
+
+
+        }
+    }
+}
