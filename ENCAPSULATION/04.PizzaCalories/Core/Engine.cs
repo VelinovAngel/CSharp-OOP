ed9c41b79e18e558e60d574c2dda97534c8ca52a
Angel <kakarum1992@gmail.com>
2020-11-05 21:06:52 +0100

    Add run class Engine

---
+++ ENCAPSULATION/04.PizzaCalories/Core/Engine.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using _04.PizzaCalories.Models;
+
+namespace _04.PizzaCalories.Core
+{
+    public class Engine
+    {
+       public void Run()
+        {
+            try
+            {
+                string[] pizzaTokens = Console.ReadLine().
+                    Split(" ");
+
+                string pizzaName = pizzaTokens[1];
+
+                string[] doughTokens = Console.ReadLine().
+                    Split(" ");
+
+                string doughName = doughTokens[1];
+                string bakeType = doughTokens[2];
+                double doughWeight = double.Parse(doughTokens[3]);
+
+                Dough dough = new Dough(doughName, bakeType, doughWeight);
+
+                Pizza pizza = new Pizza(pizzaName, dough);
+
+                string command = Console.ReadLine();
+
+                while (command.ToUpper() != "END")
+                {
+                    string[] toppingTokens = command.
+                        Split(" ");
+
+                    string toppingName = toppingTokens[1];
+                    double toppingWeight = double.Parse(toppingTokens[2]);
+
+                    Topping topping = new Topping(toppingName, toppingWeight);
+
+                    pizza.Add(topping);
+
+                    command = Console.ReadLine();
+                }
+
+                Console.WriteLine(pizza);
+
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+                return;
+            }
+        }
+    }
+}
--- ENCAPSULATION/04.PizzaCalories/Program.cs
+++ ENCAPSULATION/04.PizzaCalories/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using _04.PizzaCalories.Core;
 using _04.PizzaCalories.Models;
 
 namespace _04.PizzaCalories
@@ -8,51 +9,10 @@ namespace _04.PizzaCalories
     {
         static void Main(string[] args)
         {
-            //Dough Tip500 Chewy 100
 
-            try
-            {
-                string[] pizzaTokens = Console.ReadLine().
-                    Split(" ");
-
-                string pizzaName = pizzaTokens[1];
-
-                string[] doughTokens = Console.ReadLine().
-                    Split(" ");
-
-                string doughName = doughTokens[1];
-                string bakeType = doughTokens[2];
-                double doughWeight = double.Parse(doughTokens[3]);
-
-                Dough dough = new Dough(doughName, bakeType, doughWeight);
-
-                Pizza pizza = new Pizza(pizzaName, dough);
-
-                string command = Console.ReadLine();
-
-                while (command.ToUpper() != "END")
-                {
-                    string[] toppingTokens = command.
-                        Split(" ");
-
-                    string toppingName = toppingTokens[1];
-                    double toppingWeight = double.Parse(toppingTokens[2]);
-
-                    Topping topping = new Topping(toppingName, toppingWeight);
-
-                    pizza.Add(topping);
-
-                    command = Console.ReadLine();
-                }
-
-                Console.WriteLine(pizza);
-
-            }
-            catch (ArgumentException ae)
-            {
-                Console.WriteLine(ae.Message);
-                return;
-            }
+            Engine engine = new Engine();
+            engine.Run();
+           
         }
     }
 }
