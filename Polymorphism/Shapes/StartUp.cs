8e6be6a5c4161431660fb8d21e316d684d4f14b9
Angel <kakarum1992@gmail.com>
2020-11-11 18:54:25 +0100

    Polymorphism Shape exercise

---
+++ Polymorphism/Shapes/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Shapes.Models;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //Rectangle rect = new Rectangle(3.0, 4.0);
+
+            //Console.WriteLine(rect.CalculateArea());
+
+            //Console.WriteLine(rect.Draw());
+
+        }
+    }
+}
