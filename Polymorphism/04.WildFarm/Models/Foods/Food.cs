6933efcd90d2ba89d4fc0cd989de988359e3e74e
Angel <kakarum1992@gmail.com>
2020-11-14 23:25:25 +0100

    Cheate models for food

---
+++ Polymorphism/04.WildFarm/Models/Foods/Food.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using _04.WildFarm.Models.Foods.Interfaces;
+
+namespace _04.WildFarm.Models.Foods
+{
+    public abstract class Food : IFoods
+    {
+        protected Food(int quantity)
+        {
+            this.Quantity = quantity;
+        }
+
+        public int Quantity { get; }
+    }
+}
---
+++ Polymorphism/04.WildFarm/Models/Foods/Interfaces/IFoods.cs
@@ -0,0 +1,7 @@
+﻿using System;
+namespace _04.WildFarm.Models.Foods.Interfaces
+{
+    public interface IFoods
+    {
+    }
+}
