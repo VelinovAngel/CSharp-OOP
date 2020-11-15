3ab3a7d48d737af0b8ca9bdb6eeabcbd27e2dd17
Angel <kakarum1992@gmail.com>
2020-11-15 23:57:50 +0100

    LieutenantGeneral Class

--- INTERFACES_AND_ABSTRACTION/07.MilitaryElite/Models/LieutenantGeneral.cs
+++ INTERFACES_AND_ABSTRACTION/07.MilitaryElite/Models/LieutenantGeneral.cs
@@ -1,10 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using _07.MilitaryElite.Contracts;
+
 namespace _07.MilitaryElite.Models
 {
-    public class LieutenantGeneral
+    public class LieutenantGeneral : Private, ILieutenantGeneral
     {
-        public LieutenantGeneral()
+        private ICollection<IPrivate> privates;
+
+        public LieutenantGeneral(int id, string firstName, string lastName, decimal salary)
+            : base(id, firstName, lastName, salary)
         {
+            this.privates = new List<IPrivate>();
+        }
+
+        public IReadOnlyCollection<IPrivate> Privates
+            => (IReadOnlyCollection<IPrivate>)this.privates;
+
+        public void AddPrivate(IPrivate @private)
+        {
+            throw new NotImplementedException();
         }
     }
 }
