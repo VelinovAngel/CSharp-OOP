a82091894a837051379313ac5c0cbfa9bcd3dcc6
Angel <kakarum1992@gmail.com>
2020-11-21 20:43:50 +0100

    Add class FileAppender

---
+++ Solid/01.Logger/Models/Appenders/FileAppender.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using _01.Logger.Models.Contracts;
+using _01.Logger.Models.Enumerations;
+
+namespace _01.Logger.Models.Appenders
+{
+    public class FileAppender : IAppender
+    {
+        public FileAppender(ILayout layout , Level level, IFile file)
+        {
+            this.Layout = layout;
+            this.Level = level;
+            this.File = file;
+        }
+
+        public ILayout Layout { get; }
+
+        public Level Level { get; }
+
+        public IFile File { get;}
+
+        public void Append(IError error)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
