623b445ef14d1faff0b076bee59ead75c7ce20a4
Angel <kakarum1992@gmail.com>
2020-11-21 18:55:18 +0100

    Solid exercises

--- Solid/01.Logger/Models/Files/LogFile.cs
+++ Solid/01.Logger/Models/Files/LogFile.cs
@@ -1,21 +1,53 @@
-﻿using _01.Logger.Models.Contracts;
+﻿using System;
+using System.IO;
+using System.Linq;
+
+using _01.Logger.Models.Contracts;
+using _01.Logger.Models.Enumerations;
 
 namespace _01.Logger.Models.Files
 {
     public class LogFile : IFile
     {
+        private readonly IPathManager pathManager;
 
-        public LogFile()
+        public LogFile(IPathManager pathManager)
         {
+            this.pathManager = pathManager;
+            this.pathManager.EnsureDirectoryAndFileExists();
         }
 
-        public string Path { get; }
+        public string Path
+            => this.pathManager.CurrentFilePath;
 
-        public long Size { get; }
+        public long Size
+            => this.CalculateFileSize();
 
         public string Write(ILayout layout, IError error)
         {
-            throw new System.NotImplementedException();
+            string format = layout.Format;
+
+            DateTime dateTime = error.DateTime;
+
+            string message = error.Message;
+
+            Level level = error.Level;
+
+            string formattedMessage = string.Format(format
+                , dateTime.ToString("G"),
+                level.ToString(),
+                message.ToString());
+
+            return formattedMessage;
+        }
+
+        private long CalculateFileSize()
+        {
+            string fileText = File.ReadAllText(this.Path);
+
+            return fileText
+                .ToCharArray()
+                .Sum(c => c);
         }
     }
 }
