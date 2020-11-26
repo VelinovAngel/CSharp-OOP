e6cb0aae620e5a3a30b22bc64ea5e63ef9207348
Angel <kakarum1992@gmail.com>
2020-11-26 21:32:07 +0100

    Unit Tests

--- UNIT_TESTING/CarManager.Tests/CarTests.cs
+++ UNIT_TESTING/CarManager.Tests/CarTests.cs
@@ -1,18 +1,105 @@
 using NUnit.Framework;
+using CarManager;
+using System;
 
 namespace Tests
 {
     public class CarTests
     {
+        private Car car;
+
         [SetUp]
         public void Setup()
         {
+            this.car = new Car("make", "golf", 10, 100);
         }
 
         [Test]
-        public void Test1()
+        public void ConstructorShouldReturnCorrectlyValue()
         {
-            Assert.Pass();
+            string expMake = "make";
+            string expModel = "golf";
+            double expFuel = 10;
+            double expCapacity = 100;
+            Assert.AreEqual(expMake, car.Make);
+            Assert.AreEqual(expModel, car.Model);
+            Assert.AreEqual(expFuel, car.FuelConsumption);
+            Assert.AreEqual(expCapacity, car.FuelCapacity);
         }
+
+        [Test]
+        public void MakeShouldThrowExcpWhenIsInvalid()
+        {
+            Assert.Throws<ArgumentException>(() => new Car("", "golf", 10, 100));
+        }
+
+        [Test]
+        public void ModelShouldThrowExcpWhenIsInvalid()
+        {
+            Assert.Throws<ArgumentException>(() => new Car("make", "", 10, 100));
+        }
+
+        [Test]
+        public void TestIfFuelConsumptionIsNegativOrZero()
+        {
+            Assert.Throws<ArgumentException>(() => new Car("make", "golf", 0, 100));
+        }
+
+        //[Test]
+        //public void TestIfFuelAmountIsNegativOrZero()
+        //{
+        //    Assert.Throws<ArgumentException>(() => new Car("make", "golf", 10, 0));
+        //}
+
+        [Test]
+        public void TestIfFuelCapacityIsNegativOrZero()
+        {
+            
+            Assert.Throws<ArgumentException>(() => new Car("make", "golf", 10, 0));
+        }
+
+        [Test]
+        public void RefulMethodShouldThrowExp()
+        {
+
+            Assert.Throws<ArgumentException>(() => this.car.Refuel(-1));
+        }
+
+        [Test]
+        public void CheckIfFuelAmountIsEqualToFuelReful()
+        {
+            this.car.Refuel(1);
+            Assert.AreEqual(1, this.car.FuelAmount);
+        }
+
+        [Test]
+        public void CheckIfFuelCapcityIsEqualToFuelAmount()
+        {
+            var currCar = new Car("make", "golf", 10, 1);
+            currCar.Refuel(1);
+            Assert.AreEqual(currCar.FuelAmount, currCar.FuelCapacity);
+        }
+
+        [Test]
+        public void CheckDriveShouldThrowExp()
+        {    
+            this.car.Refuel(1);
+
+            Assert.Throws<InvalidOperationException>(() => this.car.Drive(1000));
+        }
+
+
+        [Test]
+        public void CheckFuelAmountAfterDrive()
+        {
+            //fuelConsumption = 10;
+            double exp = 9.0d;
+            this.car.Refuel(10);
+            this.car.Drive(10);
+
+            Assert.AreEqual(exp, car.FuelAmount);
+        }
+
+
     }
 }
