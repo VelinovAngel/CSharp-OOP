88241ffef3452aac09d778ff0e1f91d68ea2e17c
Angel <kakarum1992@gmail.com>
2020-11-27 21:51:00 +0100

    Unit Tests

--- UNIT_TESTING/Database.Tests/DatabaseTests.cs
+++ UNIT_TESTING/Database.Tests/DatabaseTests.cs
@@ -1,22 +1,24 @@
+using System.Linq;
 using NUnit.Framework;
 
 namespace Tests
 {
     public class DatabaseTests
     {
-        private Database database;
-
+        private Database.Database database;
 
         [SetUp]
         public void Setup()
         {
-            this.database = new Database(new int[16]);
+            this.database = new Database
+                .Database(Enumerable.Range(1, 16).ToArray());
         }
 
         [Test]
         public void ConstructorShouldReturnCorrectlyValue()
         {
             Assert.IsNotNull(this.database);
+            Assert.AreEqual(16, database.Count);
         }
     }
 }
