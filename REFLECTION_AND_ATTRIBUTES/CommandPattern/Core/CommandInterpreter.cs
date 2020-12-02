ed02f904cdefb4d9ffb025ec175eb4b619577bfb
Angel <kakarum1992@gmail.com>
2020-12-02 20:58:50 +0100

    Reflection

--- REFLECTION_AND_ATTRIBUTES/CommandPattern/Core/CommandInterpreter.cs
+++ REFLECTION_AND_ATTRIBUTES/CommandPattern/Core/CommandInterpreter.cs
@@ -1,10 +1,41 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
+
+using CommandPattern.Core.Contracts;
+
 namespace CommandPattern.Core
 {
-    public class CommandInterpreter
+    public class CommandInterpreter : ICommandInterpreter
     {
+        private const string COMMAND_POSTFIX = "Command";
         public CommandInterpreter()
         {
+        }
+
+        public string Read(string args)
+        {
+            string[] commandTokens = args
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            string commandName = commandTokens[0] + COMMAND_POSTFIX;
+
+            string[] commandArgs = commandTokens
+                .Skip(1)
+                .ToArray();
+
+            Assembly assembly = Assembly.GetCallingAssembly();
+            Type commandType = assembly
+                .GetTypes()
+                .FirstOrDefault(t => t.Name.ToLower() == commandName.ToLower());
+
+            if (commandType == null)
+            {
+                throw new ArgumentException("Invalid command type!");
+            }
+
+
         }
     }
 }
