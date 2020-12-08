fe9f603c6b708a8184f465dbe813179fc2715a6e
Angel <kakarum1992@gmail.com>
2020-12-08 19:19:56 +0100

    Reflection and attributes

--- REFLECTION_AND_ATTRIBUTES/ValidationAttributes/Utilities/Validator.cs
+++ REFLECTION_AND_ATTRIBUTES/ValidationAttributes/Utilities/Validator.cs
@@ -1,11 +1,25 @@
 ﻿using System;
+using System.Reflection;
+
 namespace ValidationAttributes.Utilities
 {
     public class Validator
     {
         public static bool IsValid(object obj)
         {
+            if (obj == null)
+            {
+                return false;
+            }
 
+            Type objType = obj.GetType();
+
+            PropertyInfo[] properties = objType.GetProperties();
+
+            foreach (PropertyInfo property in properties )
+            {
+
+            }
         }
     }
 }
