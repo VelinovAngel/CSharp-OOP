64a3df7e027eb2b086a94fe3624ca4406f02b268
Angel <kakarum1992@gmail.com>
2020-12-08 21:04:44 +0100

    Exam Prep

---
+++ Exam Prep OOP/C# OOP Retake Exam - 16 Apr 2020/01. Structure_Skeleton/RobotService/Models/Robots/Robots.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using RobotService.Models.Robots.Contracts;
+using RobotService.Utilities.Messages;
+
+namespace RobotService.Models.Robots
+{
+    public abstract class Robots : IRobot
+    {
+        private int happiness;
+        private int energy;
+        private string owner = "Service";
+        private bool isBought = false;
+        private bool isChipped = false;
+        private bool isChecked = false;
+
+
+        public string Name { get; }
+
+        public int Happiness
+        {
+            get
+            {
+                return this.happiness;
+            }
+            set
+            {
+                if (value < 0 || value > 100)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidHappiness);
+                }
+                this.happiness = value;
+            }
+        }
+
+        public int Energy
+        {
+            get
+            {
+                return this.energy;
+            }
+            set
+            {
+                if (value < 0 || value > 100)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidEnergy);
+                }
+
+                this.energy = value;
+            }
+        }
+
+        public int ProcedureTime { get; set; }
+
+        public string Owner
+        {
+            get
+            {
+                return this.owner;
+            }
+            set
+            {
+                this.owner = value;
+            }
+        }
+
+        public bool IsBought { get => this.isBought; set => value = this.isBought; }
+
+        public bool IsChipped { get => this.isChipped; set => value = this.isChipped; }
+
+        public bool IsChecked { get => this.isChecked; set => value = this.isChecked; }
+    }
+}
